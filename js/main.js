31f00eb285b5183dd947bac62a04b9723c50923a
tashinganyoni <tashinganyoni07@gmail.com>
2025-09-02 22:16:41 +0530

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// Simple main.js file for the homepage
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Flavour Meats homepage loaded successfully');
+    
+    // You can add any homepage-specific JavaScript here
+});
